3cef31cec32932c39a103ec149475fe864e1415b
Tomás García Gobet <tomasgobet@icloud.com>
2022-11-22 00:16:18 +0100

    Initial Commit

---
+++ truquito/truquitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  truquitoApp.swift
+//  truquito
+//
+//  Created by Tomás García Gobet on 22.11.22.
+//
+
+import SwiftUI
+
+@main
+struct truquitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
