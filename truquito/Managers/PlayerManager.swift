6f2bbaa861b26d2ada510f3e7b2254628287a39d
Tomás García Gobet <t@gobet.ar>
2023-02-17 11:45:22 +0100

    adds player manager viewmodel

---
+++ truquito/Managers/PlayerManager.swift
@@ -0,0 +1,42 @@
+//
+//  PlayerManager.swift
+//  truquito
+//
+//  Created by Tomás García Gobet on 16.02.23.
+//
+
+import Foundation
+import CoreData
+
+class PlayerManager: ObservableObject {
+    @Published var players: [Player] = []
+    private let context = PersistenceController.shared.container.viewContext
+    
+    func fetchAll() {
+        do {
+            self.players = try context.fetch(Player.fetchRequest())
+        } catch {
+            print("Error fetching players: \(error.localizedDescription)")
+        }
+    }
+    
+    func save() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving player: \(error.localizedDescription)")
+        }
+    }
+    
+    func add(name: String) {
+        let newPlayer = Player(context: context)
+        newPlayer.name = name
+        
+        self.save()
+    }
+    
+    func delete(_ player: Player) {
+        context.delete(player)
+        self.save()
+    }
+}
