edf056fc682697da7f67b33539f3433d4a41981d
Tomás García Gobet <41745430+reloadedhead@users.noreply.github.com>
2023-03-14 17:06:21 +0100

    Revert "remvoes gh link"
    This reverts commit ddbda27ac6fadafb0ddc9f391152ac21e7851e98.

--- truquito/Views/Settings/About/AboutView.swift
+++ truquito/Views/Settings/About/AboutView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+let GitHubUrl = "https://github.com/reloadedhead/truquito"
 let ProjectUrl = "https://github.com/users/reloadedhead/projects/1"
 let BugReportUrl = "https://github.com/reloadedhead/truquito/issues/new?assignees=&labels=&template=bug_report.md&title="
 let FeatureUrl = "https://github.com/reloadedhead/truquito/issues/new?assignees=&labels=&template=feature_request.md&title="
@@ -18,8 +19,9 @@ struct AboutView: View {
     var body: some View {
             List {
                 Section(header: Text("Truquito"), footer: Text("Si te gustó Truquito, ¡dejá una reseña cariñosa en el AppStore!")) {
-                    ExternalLinkView(url: ProjectUrl, symbolName: "checklist", label: "Roadmap público", color: .yellow)
-                    ExternalLinkView(url: BugReportUrl, symbolName: "ladybug.fill", label: "Reportar un error", color: .red)
+                    ExternalLinkView(url: GitHubUrl, symbolName: "hammer.circle", label: "Proyecto en GitHub", color: .purple)
+                    ExternalLinkView(url: ProjectUrl, symbolName: "checklist", label: "Roadmap", color: .yellow)
+                    ExternalLinkView(url: BugReportUrl, symbolName: "ladybug.fill", label: "Reportar un bug", color: .red)
                     ExternalLinkView(url: FeatureUrl, symbolName: "wand.and.stars", label: "Solicitar una idea", color: .green)
                     ExternalLinkView(url: FeatureUrl, symbolName: "heart.fill", label: "Dejar una reseña", color: .red)
                 }
